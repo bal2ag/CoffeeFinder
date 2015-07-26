146078e27b46f3fc9b7841a7496283c6ec71e368
Alex Landau <b.alex.landau@gmail.com>
2015-07-25 17:22:07 -0700

    Add wsgi file for gunicorn deployment, fetch config from env variable

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app import create_app, db
 from app.model import CoffeeShop
 from flask.ext.script import Manager, Shell
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('COFFEEFINDER_CONFIG') or 'default')
 manager = Manager(app)
 
 def make_shell_context():
